c23043165896693d65a3ded6c4fc9c05b905baf5
enamelu <sasuke18tina@gmail.com>
2022-11-06 01:23:44 +0800

    Add Step2 component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 
 import style from './App.module.scss'
 import Step1 from './components/step/Step1';
+import Step2 from './components/step/Step2';
 import Step3 from './components/step/Step3';
 import StepProgress from './components/step/StepProgress';
 
@@ -102,40 +103,9 @@ function App() {
           {/* address phase */}
           <Step1/>
           {/* shipping phase */}
-          <form className="col col-12" data-phase="shipping">
-            <h3 className="form-title">運送方式</h3>
-            <section className="form-body col col-12">
-              <label className="radio-group col col-12" data-price={0}>
-                <input
-                  id="shipping-standard"
-                  type="radio"
-                  name="shipping"
-                  defaultChecked=""
-                />
-                <div className="radio-info">
-                  <div className="col col-12">
-                    <div className="text">標準運送</div>
-                    <div className="price" />
-                  </div>
-                  <div className="period col col-12">約 3~7 個工作天</div>
-                </div>
-                <div className="radio-box-border" />
-              </label>
-              <label className="radio-group col col-12" data-price={500}>
-                <input id="shipping-dhl" type="radio" name="shipping" />
-                <div className="radio-info">
-                  <div className="col col-12">
-                    <div className="text">DHL 貨運</div>
-                    <div className="price" />
-                  </div>
-                  <div className="period col col-12">48 小時內送達</div>
-                </div>
-                <div className="radio-box-border" />
-              </label>
-            </section>
-          </form>
+          <Step2/>
           {/* credit-card phase */}
-        <Step3/>
+          <Step3/>
 
         </section>
       </section>
