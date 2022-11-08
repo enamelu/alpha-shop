8f4a3df6ba7d0515f4a33fa2a615fa25f6736ccd
enamelu <sasuke18tina@gmail.com>
2022-11-08 09:12:48 +0800

    Adjust tag code

--- src/components/step/Step1.js
+++ src/components/step/Step1.js
@@ -1,4 +1,5 @@
 export default function Step1(){
+ 
   return(
 <>
 <form className="col col-12" data-phase="address">
@@ -8,10 +9,8 @@ export default function Step1(){
                 <div className="input-group input-w-lg-2 input-w-sm-s1">
                   <div className="input-label">稱謂</div>
                   <div className="select-container">
-                    <select>
-                      <option value="mr" selected="">
-                        先生
-                      </option>
+                    <select defaultValue={'DEFAULT'}>
+                      <option value="DEFAULT">先生</option>
                       <option value="ms">女士</option>
                       <option value="mx">不明</option>
                     </select>
